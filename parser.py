89747c5182f195afc08f85e4b2648e301e19a2c5
Kevin Campusano <megakevinx@gmail.com>
2014-03-22 21:52:30 -0400

    More refinements done to the bug-related-commit detection logic.

--- parser.py
+++ parser.py
@@ -12,15 +12,15 @@ from lxml import etree
 import sys
 import os
 import csv
-from time import strptime, mktime
 from datetime import datetime
+import re
 
 output_root_path = "./output/"
 output_commit_file = "commits.xml"
 git_folder = ".git/"
 
 bug_date_format = "%Y-%m-%d %H:%M:%S"
-bug_related_words = ["bug"]
+bug_related_words = ["bug", "fix", "defect", "broken", "crash", "freeze", "break", "wrong", "glitch", "proper"]
 
 
 def commit_to_xml(commit):
@@ -78,30 +78,43 @@ def extract_commits(repos_root):
         print("> project: " + repo_name + " extraction finished")
 
 
+# Regex patterns to find bug ids according to http://dl.acm.org/citation.cfm?id=1083147
+# bug[# \t]*[0-9]+,
+# pr[# \t]*[0-9]+,
+# show\_bug\.cgi\?id=[0-9]+, or <--- wtf is this?
+# \[[0-9]+\]
+
 def are_related(commit, bug_report):
     related = False
 
-    commit_time = datetime.fromtimestamp(commit.commit_time)
+    upper_case_commit_message = commit.message.upper()
+    bug_id = bug_report["id"]
 
-    if bug_report['creation_time'] < commit_time < bug_report['closed_time']:
-        if bug_report["id"] in commit.message:
-            related = True
+    if re.search("BUG[# \s]*" + bug_id + "[ \s]+|" +
+                 "BUG[# \s]*" + bug_id + "$" + "|" +
+                 "PR[# \s]*" + bug_id + "[ \s]+" + "|" +
+                 "PR[# \s]*" + bug_id + "$" + "|" +
+                 "[ \s\D]+" + bug_id + "[ \s]+" +
+                 "^" + bug_id + "$",
+                 upper_case_commit_message):
+        related = True
 
     return related
 
 def is_bug_related(commit):
-    return any([word in commit.message for word in bug_related_words])
+    return any([word.upper() in commit.message.upper()
+                for word in bug_related_words]) or re.search("^[0-9]+$", commit.message)
 
 
 def cross_reference_commits_with_bug_reports():
-    repos_root = sys.argv[1]#"/home/kevin/Desktop/eclipse-platform"#
-    bug_reports_file = sys.argv[2]#"/home/kevin/Downloads/eclipse-bugs.csv"#
+    repos_root = "/home/kevin/Desktop/eclipse-platform"#sys.argv[1]#
+    bug_reports_file = "/home/kevin/Downloads/eclipse-bugs.csv"#sys.argv[2]#
 
-    csv_file = open(bug_reports_file, newline="")
-    bug_reports = [{"id": bug["id"],
-                    "creation_time": datetime.strptime(bug["creation_time"], bug_date_format),
-                    "closed_time": datetime.strptime(bug["closed_time"], bug_date_format)}
-                   for bug in csv.DictReader(csv_file)]
+    with open(bug_reports_file, newline="") as csv_file:
+        bug_reports = [{"id": bug["id"],
+                        "creation_time": datetime.strptime(bug["creation_time"], bug_date_format),
+                        "closed_time": datetime.strptime(bug["closed_time"], bug_date_format)}
+                       for bug in csv.DictReader(csv_file)]
 
     os.makedirs(output_root_path)
 
@@ -114,7 +127,14 @@ def cross_reference_commits_with_bug_reports():
         count = 0
 
         for bug_report in bug_reports:
-            for commit in bug_related_commits:
+
+            # This may actually hurt the detection
+            bug_related_commits_within_bug_life = \
+                [c for c in bug_related_commits
+                 if bug_report['creation_time'] <= datetime.fromtimestamp(c.commit_time) <= bug_report['closed_time']]
+
+            # for commit in bug_related_commits:
+            for commit in bug_related_commits_within_bug_life:
                 if are_related(commit, bug_report):
                     commit_xml = commit_to_xml(commit)
                     commit_xml.set("related_bug", bug_report["id"])
@@ -123,18 +143,18 @@ def cross_reference_commits_with_bug_reports():
 
             print("repo: " + repo_name + ", bug: " + bug_report["id"] + " processed")
 
+            # if count > 10:
+            #     break
+
         root.set("count", str(count))
         output_xml = xml_to_string(root)
 
         with open(os.path.join(output_root_path, repo_name + "_" + output_commit_file), "w") as file:
             file.write(output_xml)
 
-        print("> project: " + repo_name + " extraction finished")
-
 
 def main():
-    #repos_root = sys.argv[1]
-    #extract_commits(repos_root)
+    # extract_commits(sys.argv[1])
     cross_reference_commits_with_bug_reports()
 
 if __name__ == "__main__":
